9f17d43008343296a6267107fe88f4e31d3b8096
robertus adi setyadharma <adi.setyadharma83@gmail.com>
2022-06-01 08:40:51 +0700

    Update core template

---
+++ app/Views/layout/header.php
@@ -0,0 +1,20 @@
+<meta charset="utf-8">
+<title>Thomson-Minimal Portfolio Temaplate</title>
+
+<!-- mobile responsive meta -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<!-- ** Plugins Needed for the Project ** -->
+<!-- Bootstrap -->
+<link rel="stylesheet" href="<?= base_url('plugins/bootstrap/bootstrap.min.css') ?>"/>
+<link rel="stylesheet" href="<?= base_url('plugins/themify/css/themify-icons.css') ?>"/>
+<link rel="stylesheet" href="<?= base_url('plugins/counto/animate.css') ?>"/>
+<link rel="stylesheet" href="<?= base_url('plugins/aos/aos.css') ?>"/>
+<link rel="stylesheet" href="<?= base_url('plugins/owl-carousel/owl.carousel.min.css') ?>"/>
+<link rel="stylesheet" href="<?= base_url('plugins/owl-carousel/owl.theme.default.min.css') ?>"/>
+<link rel="stylesheet" href="<?= base_url('plugins/magnific-popup/magnific-popup.css') ?>"/>
+<link rel="stylesheet" href="<?= base_url('plugins/animated-text/animated-text.css') ?>"/>
+
+<!-- Main Stylesheet -->
+<link href="<?= base_url('css/style.css') ?>" rel="stylesheet"/>
